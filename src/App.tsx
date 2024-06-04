c29e442d00b2f0738d7a32844c2372f178e52608
Leonardo Arencibia <leonardoarencibia2002@gmail.com>
2024-06-04 15:32:05 -0400

    feat(Dashboard): Turn form into LoginForm component and create dashboard side panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,38 @@
-import {
-  Box,
-  Button,
-  Center,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Heading,
-  Input,
-  Stack,
-} from "@chakra-ui/react";
+import { Box, Center, Stack } from "@chakra-ui/react";
 
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
-import * as yup from "yup";
-
-type LoginFormValues = {
-  email: string;
-  password: string;
-};
-
-const validationSchema = yup.object({
-  email: yup
-    .string()
-    .email("Debe ingresar un correo electrónico válido")
-    .required("El correo es requerido"),
-  password: yup
-    .string()
-    .min(8, "La contraseña debe tener al menos 8 caracteres")
-    .required("La contraseña es requerida"),
-});
+import Header from "./Header";
+import LoginForm from "./LoginForm";
 
 function App() {
-  const {
-    register,
-    handleSubmit,
-
-    formState: { errors },
-  } = useForm<LoginFormValues>({
-    resolver: yupResolver(validationSchema),
-  });
-
-  async function submitData(data: LoginFormValues) {
-    console.log(data);
-  }
-
   return (
-    <Box width={"full"} minHeight={"100vh"}>
-      <Center width={"full"} height={"100vh"}>
-        <Stack
-          as="form"
-          padding={"5"}
-          boxShadow={"0px 4px 10px 1px #f1f1f1"}
-          spacing={"3"}
-        >
-          <Heading
-            as={"h4"}
-            size={"md"}
-            width={"full"}
-            textAlign={"center"}
-            marginBottom={"20px"}
-          >
-            Autenticarse
-          </Heading>
-          <FormControl>
-            <FormLabel>Correo UCI</FormLabel>
-            <Input
-              type="text"
-              {...register("email")}
-              isInvalid={errors.email ? true : false}
-            />
-            <FormHelperText color={"red"}>{errors.email?.message}</FormHelperText>
-          </FormControl>
-          <FormControl isInvalid={errors.password ? true : false}>
-            <FormLabel>Contrasena</FormLabel>
-            <Input
-              type="password"
-              {...register("password")}
-              isInvalid={errors.email ? true : false}
-            />
-            <FormHelperText color={"red"}>{errors.password?.message}</FormHelperText>
-          </FormControl>
-          <Button width={"full"} onClick={handleSubmit(submitData)}>
-            Enviar
-          </Button>
-        </Stack>
+    <Stack
+      width={"full"}
+      backgroundColor={"#00000033"}
+      spacing={0}
+      minHeight={"100vh"}
+    >
+      <Header />
+      <Box
+        flexGrow={1}
+        width={"full"}
+        height={"full"}
+        position={"absolute"}
+        top={0}
+        left={0}
+        filter={"blur(4px)"}
+        backgroundImage={"url(/images/login-register-background.jpg)"}
+        zIndex={-1}
+      ></Box>
+      <Center
+        width={"full"}
+        height={"full"}
+        padding={2}
+        alignItems={"center"}
+        flex={1}
+      >
+        <LoginForm />
       </Center>
-    </Box>
+    </Stack>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
+// import SidePanelMenu from "./SidePanelMenu.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
